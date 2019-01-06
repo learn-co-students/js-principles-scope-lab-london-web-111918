7ec8c7f729e72ea5aae9aad7ab1aaf5b05350068
ja9-look <janineluk95@gmail.com>
2019-01-06 21:59:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'me';
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer(){
+  return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+  return bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+  return leastFavoriteCustomer = 'you';
+}
