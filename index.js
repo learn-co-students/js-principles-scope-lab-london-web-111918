05113dc584d88b84ec247a4904971c79b4626919
jodyneckles <jody@technative.tv>
2019-01-06 16:46:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  return bestCustomer = 'not bob'; // Global scope defined within a function is not best practice. Used for illustration purposes!
+}
+
+function overwriteBestCustomer() {
+  return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = ''
+
+function changeLeastFavoriteCustomer() {
+  return leastFavoriteCustomer = 'john'
+}
